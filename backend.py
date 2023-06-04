c99d4b5c3501341a00c3372176d06e54bcfc89b1
JurajSternVukotic <jsternvuk@unipu.hr>
2023-06-04 21:35:43 +0200

    add School enum and level check

--- backend.py
+++ backend.py
@@ -4,6 +4,18 @@ from pony.orm import *
 
 db = Database()
 
+class School(Enum):
+    ABJURATION = "Abjuration"
+    CONJURATION = "Conjuration"
+    DIVINATION = "Divination"
+    ENCHANTMENT = "Enchantment"
+    EVOCATION = "Evocation"
+    ILLUSION = "Illusion"
+    NECROMANCY = "Necromancy"
+    TRANSMUTATION = "Transmutation"
+
+
+
 class Spell(db.Entity):
     id = PrimaryKey(int, auto = True)
     name = Required(str)
@@ -16,4 +28,14 @@ class Spell(db.Entity):
     description = Required(str)
     classes = Required(str)
 
+
+@property 
+def level(self):
+    return self._level
+
+@level.setter 
+def level(self, value):
+    if value < 0 or value > 9:
+        raise ValueError("Mystra banned all spells over lvl 9 after the spell plague, and negative levels don't make sense")
+    self._levle = value 
 
