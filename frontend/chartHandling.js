70009d1fa61ac8c9244f18fa4d2c3fd19922953b
JurajSternVukotic <jsternvuk@unipu.hr>
2023-06-13 12:40:20 +0200

    add more charts

--- frontend/chartHandling.js
+++ frontend/chartHandling.js
@@ -13,13 +13,51 @@ fetch('http://localhost:5000/spells')
         let labels = Object.keys(spellCountsByLevel);
         let data = Object.values(spellCountsByLevel);
 
-        let ctx = document.getElementById('chart').getContext('2d');
+        let ctx = document.getElementById('spells-by-level').getContext('2d');
         new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: labels,
                 datasets: [{
                     label: 'Number of spells by level',
+                    data: data,
+                    backgroundColor: 'rgba(140, 192, 192, 1)',
+                    borderColor: 'rgba(240, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    });
+
+fetch('http://localhost:5000/spells')
+    .then(response => response.json())
+    .then(spells => {
+        let spellCountsBySchool = {};
+        for (let spell of spells) {
+            let school = spell.school;
+            if (!(school in spellCountsBySchool)) {
+                spellCountsBySchool[school] = 0;
+            }
+            spellCountsBySchool[school]++;
+        }
+
+        let labels = Object.keys(spellCountsBySchool);
+        let data = Object.values(spellCountsBySchool);
+
+        let ctx = document.getElementById('spells-by-school').getContext('2d');
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Number of spells by school',
                     data: data,
                     backgroundColor: 'rgba(140, 192, 192, 1)',
                     borderColor: 'rgba(240, 192, 192, 1)',
